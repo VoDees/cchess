3b6809db90bb573a8a8a2e79437e0b07833897ac
David Vodak <dvodak@localhost.localdomain>
2018-08-14 09:45:04 +0200

    [FEATURE]	added clearscreen and better userspace

--- cchess.c
+++ cchess.c
@@ -8,23 +8,32 @@
 int main()
 {
 	int y_src, x_src, y_dst, x_dst;
-	char src[2] = {0};
-	char dst[2] = {0};
+	char src[4] = {0};
+	char dst[4] = {0};
+//	char mate_msg[20] = {0};
 	
+	system("clear");
 	print_chessboard();
 	while (true) {
-		scanf("%2s", src);
-		scanf("%2s", dst);
+//		if (check_mate(mate_msg)) {
+//			printf("%s", mate_msg);
+//			return 0;
+//		}
+		printf("Pohyb figurkou z: ");
+		scanf("%4s", src);
+		printf("\nNa: ");
+		scanf("%4s", dst);
 		if (parse_move(src, dst, &y_src, &x_src, &y_dst, &x_dst)) {
 			fprintf(stderr, "\nSpatnej format sefe!"
 				"dej si repete..\n");
-			continue;		
+			continue;	
 		}
 		if (move_cpiece(y_src, x_src, y_dst, x_dst)) {
 			fprintf(stderr, "\nZapsal si to nahovno!"
 				" Zkus to znova a dej do toho srdicko :)\n");
 			continue;
 		}
+		system("clear");
 		print_chessboard();
 	}
 
